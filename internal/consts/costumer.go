687593b356db28006c0aaf72cf023e8be9bdd5d3
arif kurniawan <arifkurniawan98@outlook.com>
2023-10-05 14:38:29 +0700

    feat: add constant value

--- internal/consts/costumer.go
+++ internal/consts/costumer.go
@@ -1,1 +1,8 @@
 package consts
+
+const (
+	LoanRequestStatusRequested = "Requested"
+	LoanRequestStatusApproved  = "Approved"
+	LoanRequestStatusUsed      = "Used"
+	LoanRequestStatusExpired   = "Expired"
+)
